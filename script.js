77e090da05aee02872770d4bfc4dd0336d804237
MayabaT <125461308+Sukakude@users.noreply.github.com>
2025-06-07 10:16:47 +0200

    Header added to index page, and configured it to be responsive.

---
+++ script.js
@@ -0,0 +1,26 @@
+searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+    searchForm.classList.toggle("active");
+
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove("active");
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-1').classList.add('active');
+    }
+    else{
+        document.querySelector('.header .header-1').classList.remove('active');
+    }
+}
+
+window.onload = () => {
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-1').classList.add('active');
+    }
+    else{
+        document.querySelector('.header .header-1').classList.remove('active');
+    }
+}
