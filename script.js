06c68b251f8b8711b0a626ee732ea33b82051192
MayabaT <125461308+Sukakude@users.noreply.github.com>
2025-06-07 21:26:57 +0200

    We have added the Login Page, together with it's functionality. We also added the Hero Section. IN the next phase of our project, we will try to connect a SQL database to our web application.

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 searchForm = document.querySelector(".search-form");
+let loginForm = document.querySelector(".login-form-container")
+
+var swiper = new Swiper(".slideshow", {
+    loop: true,
+    centerSlides: true,
+    autoplay:{
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints : {
+        0 : {
+            slidesPerView: 1,
+        },
+        768 : {
+            slidesPerView: 2,
+        },
+        1024 : {
+            slidesPerView: 3,
+        },
+    },
+});
+
+document.querySelector("#login-btn").onclick = () => {
+    loginForm.classList.toggle("active")
+}
 
 document.querySelector("#search-btn").onclick = () => {
     searchForm.classList.toggle("active");
 
 }
+
+document.querySelector("#close-login-btn").onclick = () =>{
+    loginForm.classList.remove("active")
+}
+
 
 window.onscroll = () => {
     searchForm.classList.remove("active");
@@ -24,3 +54,4 @@ window.onload = () => {
         document.querySelector('.header .header-1').classList.remove('active');
     }
 }
+
